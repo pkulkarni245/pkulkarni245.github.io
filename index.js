b5ae7a3abf2937d6ac5e0a6f5cbda5786247bcd9
pkulkarni245 <72600670+pkulkarni245@users.noreply.github.com>
2021-08-01 21:16:02 +0530

    404

--- index.js
+++ index.js
@@ -1,19 +1,47 @@
 $(document).ready(function(){ 
+    bttButton = document.querySelector('#back-to-top');
     $(window).scroll(function(){ 
         if ($(this).scrollTop() > 100) { 
-            $('#back-to-top').fadeIn(); 
+            bttButton.style.visibility = 'visible';
         } else { 
-            $('#back-to-top').fadeOut(); 
+            bttButton.style.visibility = 'hidden';
         } 
     }); 
 });
 
+/*
 function disableScrolling() {
     setTimeout(function() {
         document.body.style.overflow = 'hidden';
-    }, 3000);
+    }, 5000);
 }
   
 function enableScrolling() {
     document.body.style.overflow = '';
 }
+*/
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    el_autohide = document.querySelector('.autohide');
+    
+  
+    if(el_autohide){
+      var last_scroll_top = 0;
+      window.addEventListener('scroll', function() {
+            let scroll_top = window.scrollY;
+           if(scroll_top < last_scroll_top) {
+                el_autohide.classList.remove('scrolled-down');
+                el_autohide.classList.add('scrolled-up');
+            }
+            else {
+                el_autohide.classList.remove('scrolled-up');
+                el_autohide.classList.add('scrolled-down');
+            }
+            last_scroll_top = scroll_top;
+      }); 
+      // window.addEventListener
+    }
+    // if
+  
+  });
